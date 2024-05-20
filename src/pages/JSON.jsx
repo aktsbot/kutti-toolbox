54e54c66dd30ec6858573a012d12214fba5efedc
aktsbot <box.ashishk@gmail.com>
2024-05-20 23:00:13 +0530

    pretty print json

--- src/pages/JSON.jsx
+++ src/pages/JSON.jsx
@@ -1,9 +1,48 @@
+import { useState } from "react";
+
 import Page from "../components/Page";
 
 function JSONPage() {
+  const [inputStr, setInputStr] = useState("");
+  const [outputStr, setOutputStr] = useState("");
+
+  function handlePrettifyClick() {
+    try {
+      const inputJSON = JSON.parse(inputStr);
+      const jsonStr = JSON.stringify(inputJSON, undefined, 2);
+      setOutputStr(jsonStr);
+    } catch (error) {
+      setOutputStr(error.message);
+    }
+  }
+
   return (
     <Page title="JSON">
-      <h1>JSON page</h1>
+      <h1>Pretty print JSON</h1>
+
+      <div className="row">
+        <div className="col c5">
+          <textarea
+            rows="30"
+            onChange={(e) => {
+              setOutputStr("");
+              setInputStr(e.target.value);
+            }}
+            value={inputStr}
+            className="monospace"
+          />
+        </div>
+
+        <div className="col c2 text-center">
+          <button className="btn btn-sm" onClick={() => handlePrettifyClick()}>
+            Prettify &gt;
+          </button>
+        </div>
+
+        <div className="col c5">
+          <textarea rows="30" value={outputStr} className="monospace" />
+        </div>
+      </div>
     </Page>
   );
 }
